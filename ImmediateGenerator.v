8b0da31a6f63f9dcc9d42a28e5d01cf926a80492
RohanRudra <rohanrudra0914@gmail.com>
2024-10-25 19:23:36 +0530

    Immediate Changes

--- ImmediateGenerator.v
+++ ImmediateGenerator.v
@@ -8,7 +8,7 @@ module ImmGen(Opcode, instruction, ImmOutput);
         case (Opcode)
             7'b0000011: ImmOutput <= {{20{instruction[31]}}, instruction[31:20]}; // lw                                   
             7'b0100011: ImmOutput <= {{20{instruction[31]}}, instruction[31:25], instruction[11:7]}; //sw 
-            7'b1100011: ImmOutput <= {{19{instruction[31]}}, instruction[31], instruction[30:25], instruction[11:8], 1'b0}; //SB-Type
+            7'b1100011: ImmOutput <= {{19{instruction[31]}}, instruction[7], instruction[31], instruction[30:25], instruction[11:8], 1'b0}; //SB-Type
             7'b0010011: ImmOutput <= {{20{instruction[31]}}, instruction[31:20]}; //addi / ori
         endcase
     end
