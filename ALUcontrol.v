92cb7eba0f93de6b163972d98aa3ebb28c5042e0
RohanRudra <rohanrudra0914@gmail.com>
2024-12-29 20:28:54 +0530

    new commit

--- ALUcontrol.v
+++ ALUcontrol.v
@@ -5,13 +5,13 @@ module ALU_Control(fun7, fun3, ALUOp, control_out);
     output reg [3:0] control_out; //This o/p goes to ALU unit to select the specified operation
 
     always @(*) begin
-        case ({ALUOp, fun7, fun3})
+        casez ({ALUOp, fun7, fun3})
             6'b01_0_000: control_out <= 4'b0110;
             6'b10_0_000: control_out <= 4'b0010;
             6'b10_1_000: control_out <= 4'b0110;
             6'b10_0_111: control_out <= 4'b0000;
             6'b10_0_110: control_out <= 4'b0001;
-            6'b00_0_XXX: control_out <= 4'b0010; //for lw and sw
+            6'b00_0_???: control_out <= 4'b0010; //for lw and sw (fun3 is dont care)
         endcase
     end
 
